828904618ad9c01b5dc0ecfdf838ea805c26dcde
Elóra Maysa Cerutti <elora.cerutti@hotmail.com>
2019-09-10 15:41:17 -0300

    commit

--- Comandos if/Comandos if/Program.cs
+++ Comandos if/Comandos if/Program.cs
@@ -10,6 +10,25 @@ namespace Comandos_if
     {
         static void Main(string[] args)
         {
+            string nome;
+            int idade;
+            Console.WriteLine("Informe seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Informe sua idade: ");
+            idade = Convert.ToInt32(Console.ReadLine());
+
+            if (idade >= 18)
+            {
+                Console.WriteLine(nome + " Você é maior de idade");
+            }
+            else
+            {
+                Console.WriteLine(nome + " Você é menor de idade");
+
+            }
+            Console.ReadKey();
+
         }
     }
 }
