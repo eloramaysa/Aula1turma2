d95017cfdb179e09e3b61326e5bb7d4b92b55efc
Elóra Maysa Cerutti <elora.cerutti@hotmail.com>
2019-09-18 17:27:49 -0300

    commit

---
+++ MinhaPrimeiraClass/MinhaPrimeiraClass/Model/Bicicleta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaPrimeiraClass.Model
+{
+    //Bicicleta seria o objeto 
+    //O objeto é constituido por partes 
+    class Bicicleta
+    {
+        //get vai mostrar uma informação, set permite modificar o valor, ou dar a informação; 
+        public string Marca { get; set; }
+        public int Pneus { get; } = 2;
+    }
+}
---
+++ MinhaPrimeiraClass/MinhaSegundaClasse/Program.cs
@@ -0,0 +1,31 @@
+﻿using MinhaSegundaClasse.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaSegundaClasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listaDeFrutas = new List<Frutas>();
+
+            listaDeFrutas.Add(new Frutas()
+            {
+                Nome ="Banana",Quantidade =5
+            });
+            listaDeFrutas.Add(new Frutas()
+            {
+                Nome = "Tomate",
+                Quantidade = 2
+            });
+
+            listaDeFrutas.ForEach(i => Console.WriteLine($"Nome {i.Nome} Quantidade {i.Quantidade}"));
+            Console.ReadKey();
+
+        }
+    }
+}
