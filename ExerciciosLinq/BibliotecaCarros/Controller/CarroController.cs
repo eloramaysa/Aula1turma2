8ea375c91cd6192bc119b96a4fab85ece6497a58
Elóra Maysa Cerutti <elora.cerutti@hotmail.com>
2019-09-26 14:53:00 -0300

    Aula 26/09

--- ExerciciosLinq/BibliotecaCarros/Controller/CarroController.cs
+++ ExerciciosLinq/BibliotecaCarros/Controller/CarroController.cs
@@ -11,14 +11,39 @@ namespace BibliotecaCarros.Controller
     {
         private CarroContext carroDb = new CarroContext();
 
-       /* public List<Carro> RetornaListaCarro()
+        /// <summary>
+        /// Metodo para listar toda a lista dos carros
+        /// </summary>
+        /// <returns></returns>
+        public List<Carro> RetornaListaCarro()
         {
-            return carroDb.ListaDeCarros.ToList<Carro>();
-        }*/
+            return carroDb.ListaDeCarros.OrderBy(x => x.DataVenda).ToList<Carro>();
+        }
+        /// <summary>
+        /// Metodo que retorna lista de carros de acordo com o mes informado por parametro
+        /// </summary>
+        /// <param name="mes"></param>
+        /// <returns></returns>
         public  List<Carro> RelatorioMes(int mes)
         {
-            return carroDb.ListaDeCarros.FindAll(i => i.DataVenda.Month == mes);
-           
+            return carroDb.ListaDeCarros.FindAll(i => i.DataVenda.Month == mes).OrderBy(x => x.DataVenda).ToList<Carro>();
+
+
         }
+        /// <summary>
+        /// Metodo que mostra os dados da lista formatado 
+        /// </summary>
+        /// <param name="carro"></param>
+        public void MostraInformacoes(Carro carro)
+        {
+            //Template que apreseta as informações organizadas
+            string template = "Id: {0,-3} Carro: {1,8} Quantidade: {2,2} Valor: {3,7} Data de Venda: {4,10}";
+
+            string textoFormatado = string.Format(template, carro.Id, carro.CarroNome, carro.Quantidade, carro.Valor.ToString("C"), carro.DataVenda.ToShortDateString());
+
+            Console.WriteLine(textoFormatado);
+
+        }
+     
     }
 }
--- ExerciciosLinq/InterfaceCarros/Program.cs
+++ ExerciciosLinq/InterfaceCarros/Program.cs
@@ -6,30 +6,45 @@ using System.Threading.Tasks;
 using BibliotecaCarros.Controller;
 namespace InterfaceCarros
 {
-     class Program
+    class Program
     {
         static CarroController carroController = new CarroController();
         static void Main(string[] args)
         {
 
-            Console.WriteLine();
+            Console.WriteLine("----------------Relação Anual de Venda de Carros----------------------");
 
-            Console.WriteLine("Informe o mês que deseja emitir o relatório (1 a 12) ");
+            carroController.RetornaListaCarro().ForEach(i => carroController.MostraInformacoes(i));
+
+            Console.WriteLine(" => Informe o mês que deseja emitir o relatório (1 a 12) ");
             var mes = int.Parse(Console.ReadLine());
 
+            Console.Clear();
 
-           var lista= carroController.RelatorioMes(mes);
+            while (mes > 0 && mes < 13)
+            {
+                var lista = carroController.RelatorioMes(mes);
+                lista.ForEach(i => carroController.MostraInformacoes(i));
+                Console.WriteLine($"---------------------Informações sobre o Mês {mes} -------------------- - ");
 
-            Console.WriteLine($"No mês: {mes} a quantidade total de vendas foi de:" + lista.Sum(x => x.Quantidade));
-            
-            Console.WriteLine("Lucrando um valor total de: " + lista.Sum(x => x.Valor).ToString("C"));
+                Console.WriteLine($"\r\nNo mês {mes} a quantidade total de vendas foi de: " + lista.Sum(x => x.Quantidade) +" carros");
 
-            Console.WriteLine("A média de vendas nesse período foi de: " + lista.Average(x => (x.Quantidade*x.Valor)).ToString("C"));
+                Console.WriteLine("\nLucrando um valor total de: " + lista.Sum(x => x.Valor).ToString("C"));
 
-         
-            Console.ReadKey();
+                Console.WriteLine("\nA média de vendas total nesse período foi de: " + lista.Average(x => (x.Quantidade * x.Valor)).ToString("C"));
+                Console.WriteLine("\n A média unitária de vendas nesse período foi de: " + lista.Average(x => x.Valor).ToString("C"));
+
+                Console.WriteLine($"\n A maior venda do mês foi de: " + lista.Max(x => x.Quantidade) + " carro(s) do Modelo: " + lista.Max(x => x.CarroNome));
+
+                Console.WriteLine($"\n A menor venda do mês  foi de: " + lista.Min(x => x.Quantidade) + " carro(s) do Modelo: " + lista.Min(x => x.CarroNome));
+
+                Console.WriteLine("\n => Deseja saber o relatório de outro mês? Sim: Informe 1 a 12, Não: 0");
+                mes = int.Parse(Console.ReadLine());
+                Console.Clear();
+            }
+
         }
 
-        
+
     }
 }
