bbdd961586ffe9216018f23d978a38fa48b8a813
Elóra Maysa Cerutti <elora.cerutti@hotmail.com>
2019-09-30 17:23:23 -0300

    Aula 30/09

---
+++ Script/SQLQuery16.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[LocacoesDeCarros]
+(
+	[Id] INT NOT NULL PRIMARY KEY identity (1,1), 
+    [UsuarioId] INT NOT NULL, 
+    [CarroId] INT NOT NULL, 
+    [Valor ] MONEY NULL, 
+    [DataInicial ] DATETIME NULL DEFAULT GETDATE(), 
+    [DatatDevolucao] DATETIME NULL DEFAULT (DateAdd(day,5,GETDATE())),
+	[Ativo ] BIT NULL DEFAULT 1, 
+    [UsuarioCriacao] INT NULL DEFAULT 0, 
+    [UsuarioAlteracao] INT NULL DEFAULT 0, 
+    [DataCriacao] DATETIME NULL DEFAULT GETDATE(), 
+    [DataAlteracao] DATETIME NULL DEFAULT GETDATE()
+
+)
+
---
+++ Script/SQLQuery9.sql
@@ -0,0 +1,10 @@
+insert into Livros
+(Nome)
+values 
+('Livro1'),
+('Livro2'),
+('Livro3'),
+('Livro4'),
+('Livro5')
+
+select * from Livros
