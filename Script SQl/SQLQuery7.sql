def60ad2d4f676be60c43003c42438c0fddf5106
Elóra Maysa Cerutti <elora.cerutti@hotmail.com>
2019-10-02 17:23:30 -0300

    Aula 02/10

---
+++ Script SQl/SQLQuery7.sql
@@ -0,0 +1,8 @@
+﻿select * from Clientes;
+select * from Pedidos;
+
+--A partir de um numero de pedido vc consegue pegar todas as compras do cliente do pedido referenciado 
+--em ordem do maior para o menor 
+select * from Pedidos ped where ped.ClienteId in(
+select cli.Id from clientes cli inner join Pedidos ped on cli.Id = ped.ClienteId 
+where ped.Numero in ('N000001'))order by ped.Valor desc
