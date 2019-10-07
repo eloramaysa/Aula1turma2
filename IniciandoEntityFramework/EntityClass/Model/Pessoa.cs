dd20374cfe7341ed072780284ad3a42cd35474fa
Elóra Maysa Cerutti <elora.cerutti@hotmail.com>
2019-10-07 17:25:55 -0300

    commit

---
+++ IniciandoEntityFramework/EntityClass/Model/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityClass.Model
+{
+   public class Pessoa
+    {
+        //criar uma notação (Annotations)
+        
+        [Key] //Aqui temos uma notação que indica que nossa chave primára é campo abaixo no caso o Id
+        public int Id { get; set;}
+        public string Nome { get; set; }
+
+    }
+}
