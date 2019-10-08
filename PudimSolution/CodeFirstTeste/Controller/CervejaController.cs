9dd013c42e659c0120890072824bc192a00f34e0
Elóra Maysa Cerutti <elora.cerutti@hotmail.com>
2019-10-08 17:20:43 -0300

    Aula 08-10

---
+++ PudimSolution/CodeFirstTeste/Controller/CervejaController.cs
@@ -0,0 +1,20 @@
+﻿using CodeFirstTeste.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstTeste.Controller
+{
+    public class CervejaController
+    {
+        CervejaContextDb cervejaContext = new CervejaContextDb(@"data source=(localDb)\MSSQLLocalDb;initial catalog=EntityRevision.Model.CervejaContextDb;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework");
+
+        public void AddCerveja(Cerveja item)
+        {
+            cervejaContext.Cervejas.Add(item);
+            cervejaContext.SaveChanges();
+        }
+    }
+}
