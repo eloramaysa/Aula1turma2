b59382a87b7f00b85de411455ada08ad8c72c625
Elóra Maysa Cerutti <elora.cerutti@hotmail.com>
2019-11-01 17:29:07 -0300

    aula 01/11

---
+++ FirstCoreApiWeb/FirstCoreApiWeb/Models/FirstCodeContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstCoreApiWeb.Models
+{
+    public class FirstCodeContext: DbContext
+    {
+       public FirstCodeContext(DbContextOptions<FirstCodeContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Carro> Carros { get; set; }
+
+    }
+}
