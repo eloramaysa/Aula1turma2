f1f82f15510882ba3c95032df6e80c3b1c1a00f9
Elóra Maysa Cerutti <elora.cerutti@hotmail.com>
2019-11-05 14:20:14 -0300

    05/11

---
+++ WebCoreUsuarios/WebCoreUsuarios/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebCoreUsuarios.Models
+{
+    public partial class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string LoginUsu { get; set; }
+        public string Senha { get; set; }
+        public string Email { get; set; }
+        public int? UsuCriacao { get; set; }
+        public int? UsuAlteracao { get; set; }
+        public bool? Ativo { get; set; }
+        public DateTime? DataCriacao { get; set; }
+        public DateTime? DataAlteracao { get; set; }
+    }
+}
---
+++ WebCoreUsuarios/WebCoreUsuarios/Models/UsuarioBdCoreWebContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebCoreUsuarios.Models
+{
+    public partial class UsuarioBdCoreWebContext : DbContext
+    {
+        public UsuarioBdCoreWebContext()
+        {
+        }
+
+        public UsuarioBdCoreWebContext(DbContextOptions<UsuarioBdCoreWebContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Usuario> Usuario { get; set; }
+
+       
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.Property(e => e.Ativo).HasDefaultValueSql("((1))");
+
+                entity.Property(e => e.DataAlteracao)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.DataCriacao)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Email).HasMaxLength(50);
+
+                entity.Property(e => e.LoginUsu).HasMaxLength(50);
+
+                entity.Property(e => e.Nome).HasMaxLength(50);
+
+                entity.Property(e => e.Senha)
+                    .HasMaxLength(10)
+                    .IsFixedLength();
+
+                entity.Property(e => e.UsuAlteracao).HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.UsuCriacao).HasDefaultValueSql("((0))");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
